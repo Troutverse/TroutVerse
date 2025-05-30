3e2025a44041526b29ada0b148d2403475a24384
TroutVerse <trout@redbrick.space>
2025-05-30 17:40:15 +0900

    Commit Test

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,7 @@ export default function Contact() {
   return (
     <section className="mt-8">
       <h1 className="text-3xl font-bold mb-4">Contact</h1>
-      <p>Email: troutverse@gmail.com</p>
+      <div>Email: troutverse@gmail.com</div>
       <p>GitHub: <a href="https://github.com/Trout-W" className="text-blue-500" target="_blank" rel="noopener noreferrer">Trout-W</a></p>
     </section>
   );
